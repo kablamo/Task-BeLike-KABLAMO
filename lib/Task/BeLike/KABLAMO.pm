6cdc653bda8247ef737c42bda2d8acfac811df14
Eric Johnson <eric.git@iijo.org>
2012-09-30 22:14:07 +0100

    .

---
+++ lib/Task/BeLike/KABLAMO.pm
@@ -0,0 +1,157 @@
+use strict;
+use warnings;
+package Task::BeLike::KABLAMO;
+
+# VERSION
+# ABSTRACT: KABLAMO's frequently used or favorite modules
+
+=pkgroup Apps
+
+=pkg App::Ack
+
+=pkg App::cpanminus
+
+=pkg App::mymeta_requires
+
+=pkg App::Uni
+
+=pkg Devel::REPL
+
+=pkg Server::Starter
+
+
+=pkgroup CLI tools
+
+=pkg MooseX::Getopt::Usage
+
+
+=pkgroup Filesystem tools
+
+=pkg Path::Class
+
+=pkg Path::Class::Rule
+
+
+=pkgroup Database Modules
+
+=pkg DBI
+
+=pkg DBD::SQLite
+
+=pkg Fey
+
+=pkg Fey::ORM
+
+=pkg Fey::Loader
+
+
+=pkgroup Testing Modules
+
+=pkg Test::Most
+
+=pkg Test::Routine
+
+
+=pkgroup Perl development tools
+
+=pkg Data::Dumper::Concise
+
+=pkg Data::Printer
+
+=pkg Dist::Zilla
+
+=pkg Dist::Zilla::PluginBundle::DAGOLDEN
+
+=pkg Module::Load::Conditional
+
+=pkg Moose
+
+
+=pkgroup Web tools
+
+=pkg Bundle::LWP
+
+=pkg LWP::Protocol::https
+
+=pkg HTTP::Tiny
+
+=pkg IO::Socket::SSL
+
+=pkg Plack
+
+=pkg URI
+
+=pkg WWW::Mechanize
+
+=pkg Catalyst::Devel
+
+=pkg CatalystX::AuthenCookie
+
+=pkg Authen::Passphrase
+
+=pkg HTML::Scrubber
+
+=pkg Catalyst::Plugin::Unicode::Encoding
+
+=pkg Catalyst::Plugin::StackTrace
+
+=pkg Catalyst::Plugin::Log::Dispatch
+
+=pkg Catalyst::Plugin::Cache::HTTP
+
+=pkg Log::Dispatch::Config
+
+=pkg Catalyst::Plugin::Assets
+=pkg Catalyst::View::TT
+=pkg Catalyst::View::JSON
+=pkg Data::Currency
+=pkg Number::Format
+=pkg DateTime::Format::SQLite
+=pkg Plack::Middleware::Assets
+=pkg Net::Server::SS::PreFork
+=pkg Catalyst::Plugin::Compress::Gzip
+=pkg Math::Random::MT
+=pkg CEDict::Pinyin
+=pkg Data::TreeDumper
+=pkg Captcha::reCAPTCHA
+
+
+=pkgroup Data manipulation tools
+
+=pkg JSON::XS
+
+=pkg YAML::XS
+
+=pkg List::AllUtils
+
+=pkg Regexp::Common
+
+=pkg Syntax::Keyword::Junction
+
+
+=pkgroup System interaction tools
+
+=pkg Capture::Tiny
+
+=pkg IO::CaptureOutput
+
+=pkg IPC::Run3
+
+=pkg IPC::System::Simple
+
+=pkg Time::HiRes
+
+=pkg DateTime
+
+
+=pkgroup Special-purpose tools
+
+=pkg Git::Wrapper
+
+=pkg Image::ExifTool
+
+=pkg Vi::QuickFix
+
+=cut
+
+1;
